b63efce6c54ade98339e766302ba54ad60a430e1
Carol <fflorescarol@terra.com.br>
2022-12-27 19:58:45 -0300

    criando autorização por remember token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  rescue_from ::InvalidAuthenticationTokenException, with: :handle_unauthorized_request
+
+  before_action :authenticate_user!
+
+  def authenticate_user!
+    @current_user = get_current_user
+
+    raise InvalidAuthenticationTokenException.new if @current_user.nil?
+  end
+
+  def get_current_user
+    User.find_by(remember_token: remember_token) if remember_token
+  end
+
+  private
+
+  def remember_token
+    params[:session_token]
+  end
+
+  def handle_unauthorized_request
+    render json: { message: "É preciso logar para realizar essa ação." }, status: :unauthorized
+  end
+
 end
---
+++ app/exceptions/invalid_authentication_token_exception.rb
@@ -0,0 +1,2 @@
+class InvalidAuthenticationTokenException < StandardError
+end
