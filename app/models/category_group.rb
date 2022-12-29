513a13a429f86ad459da8ea4c932264ef4e39a5f
Carol <fflorescarol@terra.com.br>
2022-12-29 19:57:49 -0300

    add relacionamento com categorias

--- app/models/category_group.rb
+++ app/models/category_group.rb
@@ -1,5 +1,6 @@
 class CategoryGroup < ActiveRecord::Base
   belongs_to :user, class_name: "User", foreign_key: "user_id"
+  has_many :categories, class_name: "Category"
 
   validates :title, presence: true
   validates :category_type, inclusion: { in: CATEGORY_TYPES::ALL }
