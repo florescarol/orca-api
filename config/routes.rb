c1a316b2f37ea25d3ec1fcbd89862a94e6f632e6
Carol <fflorescarol@terra.com.br>
2022-12-29 19:58:04 -0300

    criando rota de configurações gerais

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:create]
 
+  get "settings", to: "settings#index"
+
   resources :category_groups, only: [:index, :show, :update, :create]
   get "category_groups_types", to: "category_groups#types"
 
