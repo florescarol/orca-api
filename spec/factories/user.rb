894378d96064d9aef4d503b7770266bc114d3eca
Carol <fflorescarol@terra.com.br>
2022-12-30 18:06:51 -0300

    ajustando tamanho

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user, class: "User" do
     name { Faker::Name.name }
-    username { Faker::Internet.unique.username(specifier: 10) }
+    username { Faker::Internet.unique.username(specifier: 10..20) }
     password { Faker::Internet.password(min_length: 6) }
     token { Faker::Internet.device_token }
   end
