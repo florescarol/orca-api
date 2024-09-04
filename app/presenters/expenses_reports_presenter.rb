6465d51456799aa93d8201cb0622b553671381e6
Carol <fflorescarol@terra.com.br>
2024-09-04 19:54:58 -0300

    add total amount percentage

--- app/presenters/expenses_reports_presenter.rb
+++ app/presenters/expenses_reports_presenter.rb
@@ -12,10 +12,12 @@ class ExpensesReportsPresenter < BasePresenter
           title: group.title,
           color: group.color,
           total_amount: number_to_currency_br(expenses.sum(&:amount)),
+          total_amount_percentage: (expenses.sum(&:amount).to_f / @expenses.sum(&:amount) * 100).round(2),
           categories: expenses.sort_by(&:category_name).group_by(&:category).map do |category, expenses|
             {
               name: category.name,
               total_amount: number_to_currency_br(expenses.sum(&:amount)),
+              total_amount_percentage: (expenses.sum(&:amount).to_f / @expenses.sum(&:amount) * 100).round(2),
               expenses: map_expenses(expenses)
             }
           end
@@ -31,10 +33,12 @@ class ExpensesReportsPresenter < BasePresenter
         {
           payment_date: format_date(date),
           total_amount: number_to_currency_br(expenses.sum(&:amount)),
+          total_amount_percentage: (expenses.sum(&:amount).to_f / @expenses.sum(&:amount) * 100).round(2),
           payment_methods: expenses.group_by(&:payment_method).map do |method, expenses|
             {
               name: method.name,
               total_amount: number_to_currency_br(expenses.sum(&:amount)),
+              total_amount_percentage: (expenses.sum(&:amount).to_f / @expenses.sum(&:amount) * 100).round(2),
               expenses: map_expenses(expenses)
             }
           end
@@ -49,11 +53,17 @@ class ExpensesReportsPresenter < BasePresenter
         id: expense.id,
         name: expense.name,
         amount: number_to_currency_br(expense.amount),
+        # TO DO: change amount_value to amount
+        # and amount to formatted_amount
+        # [ cant change now because of old frontend ]
+        amount_value: expense.amount,
         installments_number: expense.installments_number,
         is_installment: expense.installment?,
         date: format_date(expense.date),
         payment_date: format_date(expense.payment_date),
+        payment_method_id: expense.payment_method_id,
         payment_method_name: expense.payment_method_name,
+        category_id: expense.category_id,
         category_name: expense.category_name,
         category_group: expense.category_group_title
       }
